7efb13845141c3c991fb689bdcfbb44ce0099603
Indira <indira.mamidyala@gmail.com>
2026-07-15 13:14:21 +0530

    login fixation 1

--- vite/src/Login.jsx
+++ vite/src/Login.jsx
@@ -16,13 +16,12 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch(`${API_URL}/auth/register`, {
+      const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          
           email,
           password,
         }),
